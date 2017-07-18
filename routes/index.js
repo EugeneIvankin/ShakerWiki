24df4a3feaae79c8080bc9b5f33adf5db02fa118
EvgeniyIvankin <6733480@gmail.com>
2017-07-18 09:12:18 +0300

    add

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,10 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+
+
+router.get('/1', function (req, res) {
+    res.render ('index', { title: 'Undefain page'});
+});
+
 module.exports = router;
