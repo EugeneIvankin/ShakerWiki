f16f7e0b5be37930b79ede4d4319e94f575cb90d
EvgeniyIvankin <6733480@gmail.com>
2017-07-24 17:07:51 +0300

    add

--- bin/db.js
+++ bin/db.js
@@ -1,19 +1,19 @@
 var mysql = require('mysql');
 
-/*var client = mysql.createConnection({
+var client = mysql.createConnection({
     host: 'node44468-shakerwiki.mycloud.by',
     port: '3306',
     user: 'root',
     password: 'EGFgym01464',
     database: 'ShakerWiki'
-})*/
+})
 
-var client = mysql.createConnection({
+/*var client = mysql.createConnection({
     host: 'localhost',
     port: '3306',
     user: 'root',
     password: 'root',
     database: 'shakerwiki'
-})
+})*/
 
 module.exports = client;
