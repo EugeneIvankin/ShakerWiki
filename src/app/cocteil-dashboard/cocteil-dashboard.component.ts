1176e04c31a7170c91af75c442c75314f99a02ef
EugeneIvankin <evgeniiivankin@mail.ru>
2017-09-11 16:34:43 +0300

    add

--- src/app/cocteil-dashboard/cocteil-dashboard.component.ts
+++ src/app/cocteil-dashboard/cocteil-dashboard.component.ts
@@ -15,23 +15,37 @@ import {CocteilService} from "../services/cocteil.servece";
 export class CocteilDashboard implements OnInit{
 
   allCocteils: AllCocteils;
-  showCocteils: AllCocteils;
-  allCoc = false;
-
+  showCocteils = [];
+  numbers = [];
+  paigeOfCocteils: number;
   constructor(private cocteilService: CocteilService) {}
 
   ngOnInit(): void {
     this.cocteilService.allCocteil()
-      .subscribe((res) => {this.allCocteils = res}); //!!!!!!!!!
+      .subscribe((res) => {this.allCocteils = res; this.getNumbers(res.length/6); this.getShowCocteils(1)});
   }
 
-  getShowCocteils(res, num: number): void {
-    //for(var i=(num-1)*6; i<num*6; i++){
-      //this.allCocteils = res[0];
-      //console.log(this.allCocteils);
-   // }
-    this.allCoc = true;
+  getShowCocteils(num: number): void {
+    this.paigeOfCocteils = num;
+    if (this.showCocteils.length !==0 ){
+      for (var i=0; i<6; i++){
+        this.showCocteils.pop();
+      }
+      for(var i=(num-1)*6; i<num*6; i++){
+        this.showCocteils.push(this.allCocteils[i]);
+      }
+    }
+    else {
+      for(var i=(num-1)*6; i<num*6; i++){
+        this.showCocteils.push(this.allCocteils[i]);
+      }
+    }
   }
 
+  getNumbers(n: number){
+    for (var i=1; i<=n; i++){
+      this.numbers.push(i);
+    }
+  }
 
 }
