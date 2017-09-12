d2527da7b7b9dfbf95be5c6339a6a9cf160a8526
EugeneIvankin <evgeniiivankin@mail.ru>
2017-09-12 11:06:13 +0300

    add

--- bin/databaseConnection.js
+++ bin/databaseConnection.js
@@ -1,10 +1,18 @@
 var mysql = require('mysql');
 
-var client = mysql.createConnection({
+/*var client = mysql.createConnection({
   host: 'localhost',
   port: '3306',
   user: 'root',
   password: 'root',
+  database: 'shakerwiki'
+});*/
+
+var client = mysql.createConnection({
+  host: ' node47236-shakerwiki.mycloud.by',
+  port: '3306',
+  user: 'root',
+  password: 'NOYqcf27815',
   database: 'shakerwiki'
 });
 
